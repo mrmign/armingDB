abbf03e18d726833b4768518896f81e1c3e70400
mrmign <mrmignlee@gmail.com>
2012-12-21 00:19:14 +0800

    change some macros to private functions

--- socketwrapper.c
+++ socketwrapper.c
@@ -24,19 +24,19 @@
 #include <arpa/inet.h> /* internet socket */
 #include <string.h>
 #include "socketwrapper.h"
-// #include "socketwrapper_priv.h"
+#include "socketwrapper_priv.h"
 #include "debug.h" 
 int sockfd = -1;
 
-/*struct sockaddr_in serveraddr;                  
+struct sockaddr_in serveraddr;                  
 struct sockaddr_in clientaddr;  
 // #define MAX_CONNECT_QUEUE 1023
  //  private macro 
 void PrepareSocket(char *addr,int port)                        
  {
     debug;
-  	struct sockaddr_in serveraddr;                  
-        struct sockaddr_in clientaddr;                  
+  	// struct sockaddr_in serveraddr;                  
+   //      struct sockaddr_in clientaddr;                  
         socklen_t addr_len = sizeof(struct sockaddr);   
         serveraddr.sin_family = AF_INET;                
         serveraddr.sin_port = htons(port);              
@@ -82,48 +82,9 @@ void InitClient()
             exit(-1);                                   
         } 
 	
-}  */  
+}   
 
 
-#define PrepareSocket(addr,port)                        \
-        struct sockaddr_in serveraddr;                  \
-        struct sockaddr_in clientaddr;                  \
-        socklen_t addr_len = sizeof(struct sockaddr);   \
-        serveraddr.sin_family = AF_INET;                \
-        serveraddr.sin_port = htons(port);              \
-        serveraddr.sin_addr.s_addr = inet_addr(addr);   \
-        memset(&serveraddr.sin_zero, 0, 8);             \
-        sockfd = socket(PF_INET,SOCK_STREAM,0);         \
-        if(sockfd == -1)                                \
-        {                                               \
-            fprintf(stderr,"Socket Error,%s:%d\n",      \
-                            __FILE__,__LINE__);         \
-            close(sockfd);                              \
-            return -1;                                  \
-        } 
-#define InitServer()                                    \
-        int ret = bind( sockfd,                         \
-                        (struct sockaddr *)&serveraddr, \
-                        sizeof(struct sockaddr));       \
-        if(ret == -1)                                   \
-        {                                               \
-            fprintf(stderr,"Bind Error,%s:%d\n",        \
-                            __FILE__,__LINE__);         \
-            close(sockfd);                              \
-            exit(-1);                                   \
-        }                                               \
-        listen(sockfd,MAX_CONNECT_QUEUE);   
-
-#define InitClient()                                    \
-        int ret = connect(sockfd,                       \
-            (struct sockaddr *)&serveraddr,             \
-            sizeof(struct sockaddr));                   \
-        if(ret == -1)                                   \
-        {                                               \
-            fprintf(stderr,"Connect Error,%s:%d\n",     \
-                __FILE__,__LINE__);                     \
-            exit(-1);                                   \
-        } 
 int initialize_service(char *addr, int port)
 {
 	debug;
