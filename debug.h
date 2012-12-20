7316c90fb8535f7d41a4ac866f965b4e00e043d5
mrmign <mrmignlee@gmail.com>
2012-12-21 00:07:09 +0800

    add file, it contains some macros to print

--- debug.h
+++ debug.h
@@ -1,2 +1,22 @@
+/****************************************************************** */
+/* Copyright (C) SSE-USTC, 2012                                     */
+/*                                                                  */
+/*  FILE NAME             :  debug.h                                */
+/*  PRINCIPAL AUTHOR      :  MingLee                                */
+/*  SUBSYSTEM NAME        :  network                                */
+/*  MODULE NAME           :  tool                                   */
+/*  LANGUAGE              :  C                                      */
+/*  TARGET ENVIRONMENT    :  Linux                                  */
+/*  DATE OF FIRST RELEASE :  2012/12/14                             */
+/*  DESCRIPTION           :  the interface to Linux system(socket)  */
+/********************************************************************/
+
+/*
+ * Revision log:
+ *
+ * Created by MingLee,2012/12/14
+ *
+ */
+
 #define debug  printf("")
 // #define debug  printf("%s:%d  %s\n",__FILE__,__LINE__,__FUNCTION__)
