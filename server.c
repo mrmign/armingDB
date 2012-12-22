33060a01dcbbfa6fcf3b6b4defb6b47346d72594
mrmign <mignlee@gmail.com>
2012-12-22 21:50:50 +0800

    modify handle_requests, add handle_one_request, task_node struct, multithread

--- server.c
+++ server.c
@@ -29,27 +29,148 @@
 #include <stdint.h>
 #include "debug.h"
 #include <assert.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+
 #define PORT                5001
 #define IP_ADDR             "127.0.0.1"
 #define MAX_BUF_LEN         1024
 
+#define MAX_TASK_NUM 1
+pthread_t thread_id[MAX_TASK_NUM];
+set_m event[MAX_TASK_NUM];
+typedef struct task_node
+{
+    ServiceHandler req;// request sockfd
+    char buf[MAX_BUF_LEN];//
+    int buf_size;
 
-Database  db = NULL;
+    struct task_node *next;//next node
+    struct task_node *last;//if it is the first node, it will point to the last node,if not, it is null 
+}task_node_t;
+task_node_t *task_list[MAX_TASK_NUM];// all the task 
 
-int handle_request(ServiceHandler h);
+/* generate random int */
+int random_int(int x)
+{
+    srand((unsigned)time(0));
+    return random() % x;
+}
+
+
+Database  mdb = NULL;
+void init_mdb()
+{
+    mdb = creat_MDB();
+}
+void close_mdb()
+{
+    delete_MDB(mdb);
+}
+void match_sockfd_mdb(int sockfd, Database db)
+{
+    Data data;
+    data.value = (char *)&db;
+    data.length = sizeof(Database);
+    putKeyValue_MDB(mdb, sockfd, data);
+}
+void detach_sockfd_mdb(int sockfd)
+{
+    deleteValueByKey_MDB(mdb, sockfd);
+}
+void get_mdb(int sockfd, Database db)
+{
+    Data data;
+    data.str = (char *)&db;
+    data.length = sizeof(Database);
+    getValueByKey_MDB(mdb, sockfd, &data);
+}
+
+int handle_requests(int task_num);
+int handle_one_request(ServiceHandler h, char *buf, int buf_size);
 
 int main()
 {
     /* Server Engine for Client's Connections */
+    printf(" Arming Database Server starts!\n");
+    int i;
+    if(MAX_TASK_NUM > 0)
+    {
+        for (i = 0; i < MAX_TASK_NUM; i++)
+        {
+            sem_init(&event[i], 0, 0);
+            int temp = i;
+            if (pthread_create(&thread_id[i], NULL, (void *)handle_requests, (void *)temp) != 0)
+            {
+                printf("pthread_create error, %s:%d\n",__FILE__, __LINE__);
+                exit(-1);
+            }
+
+        }
+    }
+    /* 
+     * command line console
+     */
+     
+    init_mdb();
+
     ServiceHandler h = -1;
     initialize_service(IP_ADDR,PORT);
     while(1)
     {
         h = service_start();
-        handle_request(h);  
-        service_stop(h); 
+//        handle_request(h);  
+        task_node_t *tnode = malloc( sizeof(task_node_t));
+        tnode->buf_size = MAX_BUF_LEN;
+        if (receive_data (h, tnode->buf, &(tnode->buf_size)) == 0)
+        {
+            service_stop(h);
+            continue;
+        }
+
+        if (MAX_TASK_NUM > 0)
+        {
+            tnode->req = h;
+            int i = random_int(MAX_TASK_NUM);
+            /* put tnode into task_list */
+            if (task_list[i] == NULL)
+            {
+                task_list[i] = tnode;
+                tnode->next = NULL;
+                tnode->last = tnode;
+            }
+            else
+            {
+                task_node_t *p = task_list[i];
+                p->last->next = tnode;
+                p->last = tnode;
+                tnode->next = NULL;
+            }
+
+//        service_stop(h); 
     }
+        else 
+        {
+            handle_one_request(h, tnode->buf, tnode->buf_size);
+            free(tnode);
+        }
     shutdown_service();
+    close_mdb();
+    if (MAX_TASK_NUM > 0 )
+    {
+        for (i = 0; i< MAX_TASK_NUM; i++)
+        {
+            task_node_t *p = task_list[i];
+            task_node_t *q;
+            while (p)
+            {
+               q = p->next;
+               free(p);
+               p = q;
+            }
+        }
+    }
     return 0;
 }
 
@@ -72,17 +193,45 @@ int error_response(ServiceHandler h,char * errorinfo)
 }
 
 void printRec(mDataFormat data)
-{
+
     printf("cmd:%d\nnum:%d\nlen1:%d\nvalu1:%s\nlen2:%d\nvalu2:%s\n", data->cmd,data->value_num,data->len_value1,data->value1,data->len_value2,data->value2);
     // printf("%s:%d  %s\n",__FILE__,__LINE__,__FUNCTION__);
-}
+    }
 
 /*
  * handle user's requests
  */
-int handle_request(ServiceHandler h)
+int handle_requests(int task_num)
 {
-   
+    ServiceHandler h = -1;
+    int i = task_num;
+    while (1)
+    {
+        sem_wait(&event[i]);
+        task_node_t *pnode = NULL;
+        pnode = task_list[i];
+        h = pnode->req;
+
+        if (handle_one_request(h, pnode->buf, pnode->buf_size) == -1)
+            continue;
+
+        /* deal with pnode and its next node */
+        if (pnode->next == NULL)
+            task_list[i] = 0;
+        else
+        {
+            task_node_t *p = pnode->next;
+            p->last = pnode->last;
+            task_list[i] = p;
+        }
+        free(pnode);
+        sem_post(&event[i]);
+    }
+}
+
+int handle_one_request(ServiceHandler h, char *buf, int buf_size)
+{
+
     debug;
     char Buf[MAX_BUF_LEN] = "\0";
     int BufSize = MAX_BUF_LEN;
@@ -93,133 +242,143 @@ int handle_request(ServiceHandler h)
     data->value1 = Data1;
     data->value2 = Data2;
     /* Handle Requests of One Client */
-    while(1)
+    //    while(1)
+    //    {
+    //        BufSize = MAX_BUF_LEN;
+
+    memset(Data1, 0, MAX_BUF_LEN);
+    memset(Data2, 0, MAX_BUF_LEN);
+    data->value1 = Data1;
+    data->value2 = Data2;
+
+    /*        if(receive_data(h,Buf,&BufSize) == 0)
+              {
+              fprintf(stderr,"Connection Error,%s:%d\n",__FILE__,__LINE__);
+              return -1;            
+              }*/
+    if(BufSize == 0)
     {
+        return -1;
+    }
+    int ret = parse_data(buf,data);
+
+    // print the received data
+    // printRec(data);
+
+    if(ret == -1)
+    {
+        error_response(h,"Data Format Error!\n");
+        printf("server parse_data error!\n");
+        return -1;
+    }
+    if(data->cmd == OPEN_CMD)
+    {
+        debug;
+        Database db = createNewDB(data->value1);
+        match_sockfd_mdb(h, db);
         BufSize = MAX_BUF_LEN;
+        data->value_num = 0;
+        BufSize = format_data(Buf,data);
+        assert(BufSize > 0);
+        send_data(h,Buf,BufSize);
+    }
+    else if(data->cmd == CLOSE_CMD)
+    {
+        Database db = NULL;
+        get_mdb(h, db);
+        closeDB(db);
+        BufSize = MAX_BUF_LEN;
+        data->value_num = 0;
+        BufSize = format_data(Buf,data);
+        send_data(h,Buf,BufSize);
+        return 0;
+    }
+    else if(data->cmd == GET_CMD)
+    {
+        /*if(Data1Size != sizeof(tKey))
+          {
+          fprintf(stderr,"Data Format Error,%s:%d\n",__FILE__,__LINE__);
+          continue;            
+          }*/
 
-        memset(Data1, 0, MAX_BUF_LEN);
-        memset(Data2, 0, MAX_BUF_LEN);
-        data->value1 = Data1;
-        data->value2 = Data2;
+        Database db = NULL;
+        get_mdb(h, db);
+        int key = *(int *)data->value1;
+        Data value;
 
-        if(receive_data(h,Buf,&BufSize) == 0)
-        {
-            fprintf(stderr,"Connection Error,%s:%d\n",__FILE__,__LINE__);
-            return -1;            
-        }
-        if(BufSize == 0)
-        {
-            continue;
-        }
-        int ret = parse_data(Buf,data);
-
-        // print the received data
-        // printRec(data);
-        
+        char tem[MAX_BUF_LEN] = "\0";
+        value.value = tem;
+        ret = getValueByKey(db, key, &value);
+        // printf("server getdata value => %s\n", value.value);
         if(ret == -1)
         {
-            error_response(h,"Data Format Error!\n");
-            printf("server parse_data error!\n");
-            continue;
+            error_response(h,"The key NOT FOUND!\n");
+            return -1;              
         }
-        if(data->cmd == OPEN_CMD)
-        {
-            debug;
-            db = createNewDB(data->value1);
-            BufSize = MAX_BUF_LEN;
-            data->value_num = 0;
-            BufSize = format_data(Buf,data);
-            assert(BufSize > 0);
-            send_data(h,Buf,BufSize);
-        }
-        else if(data->cmd == CLOSE_CMD)
-        {
-            closeDB(db);
-            BufSize = MAX_BUF_LEN;
-            data->value_num = 0;
-            BufSize = format_data(Buf,data);
-            send_data(h,Buf,BufSize);
-            return 0;
-        }
-        else if(data->cmd == GET_CMD)
-        {
-            /*if(Data1Size != sizeof(tKey))
-            {
-                fprintf(stderr,"Data Format Error,%s:%d\n",__FILE__,__LINE__);
-                continue;            
-            }*/
+        BufSize = MAX_BUF_LEN;
+        data->value_num = 1;
+        data->len_value1 = strlen(value.value);
+        data->value1 = value.value;
+        BufSize = format_data(Buf,data);
+        send_data(h,Buf,BufSize);
 
-            int key = *(int *)data->value1;
-            Data value;
- 
-            char tem[MAX_BUF_LEN] = "\0";
-            value.value = tem;
-            ret = getValueByKey(db, key, &value);
-            // printf("server getdata value => %s\n", value.value);
-            if(ret == -1)
-            {
-                error_response(h,"The key NOT FOUND!\n");
-                continue;
-            }
-            BufSize = MAX_BUF_LEN;
-            data->value_num = 1;
-            data->len_value1 = strlen(value.value);
-            data->value1 = value.value;
-            BufSize = format_data(Buf,data);
-            send_data(h,Buf,BufSize);
-           
-            // Buf[0]=0;
-            // Data1[0]=0;
-            // Data2[0]=0;
-        }
-        else if(data->cmd == SET_CMD)
+        // Buf[0]=0;
+        // Data1[0]=0;
+        // Data2[0]=0;
+    }
+    else if(data->cmd == SET_CMD)
+    {
+        /*if(Data1Size != sizeof(tKey))
+          {
+          fprintf(stderr,"Data Format Error,%s:%d\n",__FILE__,__LINE__);
+          continue;            
+          }*/
+        int key = *(int *)data->value1;
+        Data value;
+        // value.len = Data2Size;
+        value.value = data->value2;
+        value.length = data->len_value2;
+        // debug("SET_CMD:%d -> %s\n",*(tKey*)(Buf + 12),(char*)(Buf + 20));
+        // debug("SET_CMD:%d -> %s\n",key,value.str);
+        Database db = NULL;
+        get_mdb(h, db);
+        ret = putKeyValue(db,key,&value);
+        if (ret == -1)
         {
-            /*if(Data1Size != sizeof(tKey))
-            {
-                fprintf(stderr,"Data Format Error,%s:%d\n",__FILE__,__LINE__);
-                continue;            
-            }*/
-            int key = *(int *)data->value1;
-            Data value;
-            // value.len = Data2Size;
-            value.value = data->value2;
-            value.length = data->len_value2;
-            // debug("SET_CMD:%d -> %s\n",*(tKey*)(Buf + 12),(char*)(Buf + 20));
-            // debug("SET_CMD:%d -> %s\n",key,value.str);
-            ret = putKeyValue(db,key,&value);
-            if (ret == -1)
-            {
-                error_response(h,"Save key & value error!\n");
-                printf("set error : %d, %s\n", key, value.value);
-            }
-            BufSize = MAX_BUF_LEN;
-            data->value_num = 0;
-            BufSize = format_data(Buf,data);
-            send_data(h,Buf,BufSize);    
-                      
+            error_response(h,"Save key & value error!\n");
+            printf("set error : %d, %s\n", key, value.value);
         }
-        else if(data->cmd == DEL_CMD)
+        BufSize = MAX_BUF_LEN;
+        data->value_num = 0;
+        BufSize = format_data(Buf,data);
+        send_data(h,Buf,BufSize);    
+
+    }
+    else if(data->cmd == DEL_CMD)
+    {
+        /*if(Data1Size != sizeof(tKey))
+          {
+          fprintf(stderr,"Data Format Error,%s:%d\n",__FILE__,__LINE__);
+          continue;            
+          }*/
+        int key = *(int *)data->value1;;           
+        Database db = NULL;
+        get_mdb(h, db);
+        ret = deleteValueByKey(db,key);
+        if(ret == -1)
         {
-            /*if(Data1Size != sizeof(tKey))
-            {
-                fprintf(stderr,"Data Format Error,%s:%d\n",__FILE__,__LINE__);
-                continue;            
-            }*/
-            int key = *(int *)data->value1;;           
-            ret = deleteValueByKey(db,key);
-            if(ret == -1)
-            {
-                error_response(h,"The key NOT FOUND!\n");
-                continue;
-            }            
-            BufSize = MAX_BUF_LEN;
-            data->value_num = 0;
-            BufSize = format_data(Buf,data);
-            send_data(h,Buf,BufSize);             
-        }
-        else
-        {
-            printf("Unknow Request!\n");
-        }                                   
-    }    
+            error_response(h,"The key NOT FOUND!\n");
+            continue;
+        }            
+        BufSize = MAX_BUF_LEN;
+        data->value_num = 0;
+        BufSize = format_data(Buf,data);
+        send_data(h,Buf,BufSize);             
+    }
+    else
+    {
+        printf("Unknow Request!\n");
+        error_response(h, "Unknow Request!\n");
+    }                                   
+    //    }    
 }
