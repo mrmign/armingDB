149b2ef3617bde71ecf2b3b08595619b8dc19e4e
mrmign <mignlee@gmail.com>
2013-01-11 13:12:13 +0800

    add parse control interface and implementation

--- protocol.c
+++ protocol.c
@@ -22,8 +22,9 @@
 #include <stdlib.h> 
 #include <string.h>
 #include "protocol.h"
-#include "debug.h"
+//#include "debug.h"
 
+#define debug 	printf
 // typedef struct DataHeader
 // {
 // 	int cmd;
@@ -74,6 +75,30 @@ int format_data( char *buf, mDataFormat data)
 
 }
 
+/* format the control data */
+int format_ctl_data(char *pBuf,int *pBufSize,int cmd,char ppData[MAX_DATA_NUM][MAX_DATA_LEN],int DataNum)
+{
+	int i = 0;
+    int DataSize = 0;
+    for(i=0;i<DataNum;i++)
+    {
+        DataSize += strlen(ppData[i]);
+    }
+   
+    mDataFormat pData = (mDataFormat)pBuf;
+    pData->cmd = htonl(cmd);
+    pData->value_num = htonl(DataNum);
+    pData->len_value1 = htonl(0);
+    char * p = pBuf + 2 * sizeof(int); /* point to pData->len_value1 */
+    for(i=0;i<DataNum;i++)
+    {
+        *(int*)p = strlen(ppData[i]);
+        p += sizeof(int);        
+        memcpy(p,ppData[i],strlen(ppData[i]));
+        p += strlen(ppData[i]);
+    }     
+    return strlen(pBuf); 
+}
 /*
  * parse data
  */
@@ -122,4 +147,47 @@ int parse_data(char *buf, mDataFormat data)
 		printf("Parse Error !\n");
 		return -1;
 	}
+}
+
+/* parse control data */
+int parse_ctl_data(char *pBuf,int BufSize,int *pcmd,int *pDataNum,char ppData[MAX_DATA_NUM][MAX_DATA_LEN])
+{
+	mDataFormat pData = (mDataFormat)pBuf;
+    *pcmd = ntohl(pData->cmd);
+    *pDataNum = ntohl(pData->value_num);
+    debug("ParseDataN CMD:%d,DataNum:%d\n",*pcmd,*pDataNum);
+    if(*pDataNum > MAX_DATA_NUM)
+    {
+        fprintf(stderr,"Parse control Error:*pDataNum > MAX_DATA_NUM,%s:%d\n", __FILE__,__LINE__);
+        return -1;        
+    }
+    int DataSize = 0;
+    char * p = pBuf + 2 * sizeof(int);
+    int i = 0;
+    for(i=0;i<(*pDataNum);i++)
+    {
+        DataSize = *(int*)p;
+        debug("ParseDataN:%d,%s\n",DataSize,p + sizeof(int));
+        if(DataSize > MAX_DATA_LEN)
+        {
+            fprintf(stderr,"Parse control Error:DataSize > MAX_DATA_LEN,%s:%d\n", __FILE__,__LINE__);
+            return -1;        
+        }        
+        p += sizeof(int);
+        memcpy(ppData[i],p,DataSize);
+        ppData[i][DataSize] = '\0';
+        debug("ParseDataN:%d,%s\n",DataSize,ppData[i]);
+        p += DataSize;
+    }
+    return 0;   
+}
+
+/* parse cmd */
+int parse_cmd(char * pBuf,int BufSize,int *pcmd,int *pDataNum)
+{
+	mDataFormat pData = (mDataFormat)pBuf;
+    *pcmd = ntohl(pData->cmd);
+
+    *pDataNum = ntohl(pData->value_num);
+    return 0;  
 }
--- protocol.h
+++ protocol.h
@@ -26,6 +26,12 @@
 #define GET_CMD		4
 #define DEL_CMD		5
 #define ERROR_RESP  0
+
+#define CTL_CMD 	10
+#define CTL_REG_CMD 11
+
+#define MAX_DATA_NUM    128
+#define MAX_DATA_LEN    128
 typedef struct DataFormat
 {
 	int cmd;
@@ -48,6 +54,15 @@ typedef struct DataFormat
 int format_data(char *buf, mDataFormat data);
 
 /*
+ * Format the  control data to be sent
+ * input	: buf, bufsize,cmd, data, dataNum
+ * output	: None
+ * in/out	: buf, the formated data to be sent
+ * return	: if SUCCESS return the size of the data to be sent
+ 			: if FAIL return  -1 
+ */
+int format_ctl_data(char *pBuf,int *pBufSize,int cmd,char ppData[MAX_DATA_NUM][MAX_DATA_LEN],int DataNum);
+/*
  * parse the data received from net
  * input	: buf, data
  * output	: None
@@ -56,5 +71,25 @@ int format_data(char *buf, mDataFormat data);
  			: if FAIL    return -1
  */
 int parse_data(char *buf, mDataFormat data);
+
+/*
+ * parse the control data received from server
+ * input	: buf, bufsize, data, dataNum
+ * output	: None
+ * in/out	: data, parse the data from net format to local formt
+ * return	: if SUCCESS return 0
+ 			: if FAIL    return -1
+ */
+int parse_ctl_data(char *pBuf,int BufSize,int *pcmd,int *pDataNum,char ppData[MAX_DATA_NUM][MAX_DATA_LEN]);
+
+/*
+ * parse the cmd in data header
+ * input	: buf, data
+ * output	: None
+ * in/out	: data, parse the data from net format to local formt
+ * return	: if SUCCESS return 0
+ 			: if FAIL    return -1
+ */
+int parse_cmd(char * pBuf,int BufSize,int *pcmd,int *pDataNum);
 
 #endif /* _PROTOCOL_H_*/ 
